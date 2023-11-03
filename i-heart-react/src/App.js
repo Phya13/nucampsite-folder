248cf767759e8911cc8f37fd7939aeefb3b0802f
lamnu <tuanpiang98@gmail.com>
2023-11-03 10:00:48 -0500

    updates

---
+++ i-heart-react/src/App.js
@@ -0,0 +1,14 @@
+import HeartsList from "./components/HeartsLIst";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeartsList />
+    </>
+  );
+}
+
+export default App;
--- nucampsite/src/features/campsites/CampsitesList.js
+++ nucampsite/src/features/campsites/CampsitesList.js
@@ -1,20 +1,18 @@
+import { useSelector } from "react-redux";
 import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsties } from "./campsitesSlice";
 // import { CAMPSITES } from "../../app/shared/CAMPSITES";
 
 const CampsitesList = () => {
-  const campsites = selectAllCampsties();
+  const campsites = useSelector(selectAllCampsties);
+  console.log("campsites:", campsites);
 
   return (
     <Row className="ms-auto">
       {campsites.map((campsite) => {
         return (
-          <Col
-            md="5"
-            className="m-4"
-            key={campsite.id}
-          >
+          <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
           </Col>
         );
